0cc5e6d45de005ccc98365222658f96b236a56e8
Travis Barber <tbarber350@gmail.com>
2016-09-05 10:09:14 -0600

    setup gulp add font

---
+++ familia/gulpfile.js
@@ -0,0 +1,20 @@
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('sass', function () {
+  'use strict';
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:watch', function () {
+  'use strict';
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
